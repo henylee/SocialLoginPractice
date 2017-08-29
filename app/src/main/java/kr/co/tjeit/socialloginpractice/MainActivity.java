2e0ae37b04f143b3315941f16e5b70c75d3da99b
cho881020 <cho881020@gmail.com>
2017-08-29 10:37:11 +0900

    자체 로그인 연습

--- app/src/main/java/kr/co/tjeit/socialloginpractice/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/socialloginpractice/MainActivity.java
@@ -38,27 +38,36 @@ public class MainActivity extends BaseActivity {
 
 //                존재하지 않는 아이디입니다. (아이디 X)
 //                비밀번호가 올바르지 않습니다. (아이디 O 비번 X)
-                
-                boolean loginOk = false;
-                
+
+                boolean idOk = false;
+                boolean pwOk = false;
 
 //                모든 사용자에 대해 검사하기 위한 반복문
                 for (User user : GlobalData.allUsers) {
 //                    입력된 아이디가 검사하는 사용자의 아이디와 같은지 검사하는 조건문
                     if (user.getUserId().equals(idEdt.getText().toString()) ) {
 //                        아이디가 존재하는 상황에서, 비밀번호가 일치하는지 검사하는 조건문
+
+                        idOk = true;
+
                         if (user.getPassword().equals(pwEdt.getText().toString())) {
 //                            아이디도 일치하고, 비밀번호도 일치하므로 로그인 성공 Toast
-                            loginOk = true;
+                            pwOk = true;
                         }
                     }
                 }
                 
-                if (loginOk) {
+                if (idOk && pwOk) {
                     Toast.makeText(mContext, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show();
                 }
                 else {
-                    Toast.makeText(mContext, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                    if (idOk) {
+                        Toast.makeText(mContext, "비밀번호가 올바르지 않습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(mContext, "존재하지 않는 아이디입니다.", Toast.LENGTH_SHORT).show();
+                    }
+
                 }
 
             }
