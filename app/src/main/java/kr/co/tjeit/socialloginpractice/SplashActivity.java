d2f935b281fbeec4a4e85641f2365534fe77acfc
cho881020 <cho881020@gmail.com>
2017-08-29 13:11:22 +0900

    페이스북 로그인 준비작업

--- app/src/main/java/kr/co/tjeit/socialloginpractice/SplashActivity.java
+++ app/src/main/java/kr/co/tjeit/socialloginpractice/SplashActivity.java
@@ -1,9 +1,17 @@
 package kr.co.tjeit.socialloginpractice;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import kr.co.tjeit.socialloginpractice.util.ContextUtil;
 
@@ -13,6 +21,21 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "kr.co.tjeit.socialloginpractice",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
